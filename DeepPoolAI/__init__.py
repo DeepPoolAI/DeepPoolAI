c8da637be71a864f1f9f6bf65883e490d2a821fe
Bartlomiej Eljasiak <Bartlomiej_Eljasiak@users.noreply.github.com>
2020-12-11 19:01:17 +0100

    pool db init

--- DeepPoolAI/__init__.py
+++ DeepPoolAI/__init__.py
@@ -1,10 +1,12 @@
 from .SateliteImages.object import AerialImage, GridPhotos
 from .PoolDetector.object import PoolDetector
 from .PoolAddressParser.object import PoolAddressParser
+from .PoolDatabase.object import PoolDatabase
 
 __all__ = [
     "AerialImage",
     "GridPhotos",
+    "PoolDatabase",
     "PoolDetector",
     "PoolAddressParser"
 ]
